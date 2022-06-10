4710f47ea24c741ae1ae58649b493abe034dd878
Muhammad-Rayhan <rayhan.cse2k17@gmail.com>
2022-06-10 22:52:39 +0600

    Create second page

---
+++ 07_PHP_Basic_to_Advance/01_Echo_Print.php
@@ -0,0 +1,12 @@
+<?php
+    echo "<strong><i>Hello\n"."Programmer</i></strong><br>";
+    echo 20.5;
+    echo '<br>','<br>';
+
+    print "Hello\n"."Rayhan";
+
+    //print "Hello\n","Rayhan";   (Multipal Argument is not show in print)
+
+    // . = concatenate (Means = শ্রেণীবদ্ধভাবে সংযুক্ত করা) (echo "Hello","Programmer") There are one argument in echo function.
+    //, = Argument (echo "Hello","Programmer") There are two deference argument(যুক্তি) in echo function. But output show in one line.
+?>
